0d8e9212a250188e546d7d2cc7f2b4a93dd8015c
Mils7023 <milan.nariya.095@gamil.com>
2023-05-29 19:08:19 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -339,23 +339,20 @@ export async function getStaticProps(context) {
   };
 }
 
-// export async function handler(req, res) {
-//   console.log("res" , req)
-//   // Check for secret to confirm this is a valid request
-//   if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
-//     return res.status(401).json({ message: 'Invalid token' });
-//   }
+export async function handler(req, res) {
+  console.log("res" , req)
+  // Check for secret to confirm this is a valid request
+  if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
  
-//   try {
-//     // this should be the actual path not a rewritten path
-//     // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-//     // await res.revalidate('/path-to-revalidate');
-//     await res.revalidate(req.query.path);
-//     console.log("process.env.MY_SECRET_TOKEN", process.env.MY_SECRET_TOKEN)
-//     return res.json({ revalidated: true });
-//   } catch (err) {
-//     // If there was an error, Next.js will continue
-//     // to show the last successfully generated page
-//     return res.status(500).send('Error revalidating');
-//   }
-// }
+  try {
+    await res.revalidate(req.query.path);
+    console.log("process.env.MY_SECRET_TOKEN", process.env.MY_SECRET_TOKEN)
+    return res.json({ revalidated: true });
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send('Error revalidating');
+  }
+}
