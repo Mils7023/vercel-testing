0201e0f56594f1df25e0e4b783715d5d83032687
Mils7023 <milan.nariya.095@gamil.com>
2023-05-30 10:44:06 +0530

    revalidation changes

--- pages/services/ecommerce.js
+++ pages/services/ecommerce.js
@@ -214,20 +214,4 @@ export async function getStaticProps(context) {
 }
 
 
-export async function handler(req, res) {
-  console.log("res" , req)
-  // Check for secret to confirm this is a valid request
-  if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
-    return res.status(401).json({ message: 'Invalid token' });
-  }
- 
-  try {
-    await res.revalidate("/services/ecommerce");
-    console.log("process.env.MY_SECRET_TOKEN", process.env.MY_SECRET_TOKEN)
-    return res.json({ revalidated: true });
-  } catch (err) {
-    // If there was an error, Next.js will continue
-    // to show the last successfully generated page
-    return res.status(500).send('Error revalidating');
-  }
-}
+
