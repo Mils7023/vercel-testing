281bd5b2e95c73f68d78ea6cece78be6b8a303f2
Mils7023 <milan.nariya.095@gamil.com>
2023-05-30 10:27:12 +0530

    ondemand revalidation

--- pages/services/ecommerce.js
+++ pages/services/ecommerce.js
@@ -213,36 +213,21 @@ export async function getStaticProps(context) {
   };
 }
 
-// menu(id: "react-header", idType: SLUG) {
-//     menuItems(where: { parentId: "0" }) {
-//       nodes {
-//         url
-//         label
-//         childItems {
-//           nodes {
-//             url
-//             label
-//             cssClasses
-//           }
-//         }
-//         cssClasses
-//       }
-//     }
-//   }
 
-// menu(id: "live-menu", idType: SLUG) {
-//   menuItems(where: {parentId: "0"}) {
-//     nodes {
-//       url
-//       label
-//       childItems {
-//         nodes {
-//           url
-//           label
-//           cssClasses
-//         }
-//       }
-//       cssClasses
-//     }
-//   }
-// }
+export async function handler(req, res) {
+  console.log("res" , req)
+  // Check for secret to confirm this is a valid request
+  if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+ 
+  try {
+    await res.revalidate("/services/ecommerce");
+    console.log("process.env.MY_SECRET_TOKEN", process.env.MY_SECRET_TOKEN)
+    return res.json({ revalidated: true });
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send('Error revalidating');
+  }
+}
